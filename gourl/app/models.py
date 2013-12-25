2a255d3046161fa26ab3c324f88792517d8a23ae
Denver Coneybeare <denver@sleepydragon.org>
2013-12-25 09:07:08 -0500

    gourl/app/models.py: added UrlEntry and UrlInfo models

--- gourl/app/models.py
+++ gourl/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class UrlEntry(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class UrlInfo(models.Model):
+    url_entry = models.ForeignKey(UrlEntry)
+    version = models.IntegerField()
+    url = models.CharField(max_length=4096)
+    visible = models.BooleanField()
