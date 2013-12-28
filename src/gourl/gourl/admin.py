9d3f7540729d9d19eb1dbfa69b4457f593a037bb
Denver Coneybeare <denver@sleepydragon.org>
2013-12-28 05:58:01 -0500

    src/gourl/gourl/admin.py: pretty up the admin site for the Url model

--- src/gourl/gourl/admin.py
+++ src/gourl/gourl/admin.py
@@ -5,4 +5,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from gourl.gourl.models import Url
+
+
+class UrlAdmin(admin.ModelAdmin):
+    fields = ["name", "url"]
+    list_display = ["name", "url"]
+    search_fields = ["name", "url"]
+
+admin.site.register(Url, UrlAdmin)
